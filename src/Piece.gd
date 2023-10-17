3b69c35ab6daa19ce79a4d91e6181c61ebb6929f
JoJo <jo@jo.zone>
2023-10-17 16:18:11 +0200

    Piece scene with both colors & all roles. No logic yet

---
+++ src/Piece.gd
@@ -0,0 +1,28 @@
+@tool
+extends Sprite2D
+
+@export var role: Role:
+	set(r): role = r; update_sprite()
+@export var white: bool:
+	set(w): white = w; update_sprite()
+
+enum Role {
+	KING,
+	QUEEN,
+	BISHOP,
+	KNIGHT,
+	ROOK,
+	PAWN
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func update_sprite():
+	frame_coords = Vector2i(role, 0 if white else 1)
+	queue_redraw()
